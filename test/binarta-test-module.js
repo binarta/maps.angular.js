78839c42d05b8ebc3acf39225956368fe50f2b2f
wro <wouter@websters.be>
2018-11-06 14:03:40 +0100

    fix failing specs

--- test/binarta-test-module.js
+++ test/binarta-test-module.js
@@ -1,4 +1,5 @@
 (function () {
+    angular.module('binarta-alljs-tpls-angular1', []);
     angular.module('binarta-applicationjs-gateways-angular1', ['binarta-applicationjs-inmem-angular1'])
         .provider('binartaApplicationGateway', ['inmemBinartaApplicationGatewayProvider', proxy]);
 
